b54ef85c67982ef1134e9eb253a51b791533d661
Satyam Kumar <silentvoice4143@gmail.com>
2023-11-26 19:07:44 +0530

    final home

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,95 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { FaDatabase, FaDownload, FaPerson } from "react-icons/fa6";
+import { BsGlobe2 } from "react-icons/bs";
+import { BsDatabaseFillCheck } from "react-icons/bs";
+import {
+  FaSitemap,
+  FaRegArrowAltCircleRight,
+  FaFileCode,
+  FaGithub,
+  FaNodeJs,
+} from "react-icons/fa";
+import {} from "react-icons/fa6";
+import { MdOutlineManageAccounts } from "react-icons/md";
+import { RiSpeakFill } from "react-icons/ri";
+import adduser from "../email";
 
-export class About extends Component {
-  static propTypes = {};
+function About() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
 
-  render() {
-    return <div>About</div>;
+  function handleuser() {
+    adduser(name, email, message);
+    setEmail("");
+    setName("");
+    setMessage("");
   }
+  return (
+    <div className="about-container">
+      <div
+        className="contact-section py-[6rem] px-[15%] bg-lightgray font-roboto relative"
+        id="Contact"
+      >
+        <div className="connect-box w-[40vw] flex flex-col gap-4">
+          <h1 className="text-pred text-[4rem] font-semibold">
+            Connect with me
+          </h1>
+          <input
+            type="text"
+            name="name"
+            id=""
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            value={name}
+            placeholder="Name"
+            className="bg-lightergray p-4 text-gray-500 outline-none"
+          />
+          <input
+            type="email"
+            name="email"
+            id=""
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            placeholder="Email"
+            className="bg-lightergray p-4 text-gray-500 outline-none"
+          />
+
+          <input
+            type="text"
+            name="msg"
+            id=""
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            value={message}
+            placeholder="Message"
+            className="bg-lightergray p-4 text-gray-500 outline-none"
+          />
+          <div className="flex w-full justify-end text-lightgray text-[1.2rem]">
+            <button
+              onClick={handleuser}
+              className="bg-pred p-4 flex gap-4 items-center justify-center font-bold"
+            >
+              Stay Connected
+              <FaRegArrowAltCircleRight />
+            </button>
+          </div>
+        </div>
+        <div className="line absolute w-[2px] bg-gray-400 top-[30%] right-[20%] h-[250px] max-[770px]:hidden">
+          <div className="circle1 w-8 h-8 border-2 border-pred bg-lightgray rounded-full absolute top-0 right-[-14.8px] z-2"></div>
+          <div className="circle2 w-8 h-8 border-2 border-pred bg-pred rounded-full absolute top-[43%] right-[-14.8px]"></div>
+          <div className="circle3 w-8 h-8 border-2 border-pred bg-lightgray rounded-full absolute bottom-0 right-[-14.8px]"></div>
+        </div>
+      </div>
+      <div className="copyright bg-black h-[3rem] flex justify-center items-center">
+        <p>&copy; Satyam | Developer | Programming Hunter 2023</p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -1,7 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { FaDatabase, FaDownload, FaPerson } from "react-icons/fa6";
+import { BsGlobe2 } from "react-icons/bs";
+import { BsDatabaseFillCheck } from "react-icons/bs";
+import {
+  FaSitemap,
+  FaRegArrowAltCircleRight,
+  FaFileCode,
+  FaGithub,
+  FaNodeJs,
+} from "react-icons/fa";
+import {} from "react-icons/fa6";
+import { MdOutlineManageAccounts } from "react-icons/md";
+import { RiSpeakFill } from "react-icons/ri";
+import adduser from "../email";
 
 function Service() {
-  return <div>Service</div>;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleuser() {
+    adduser(name, email, message);
+    setEmail("");
+    setName("");
+    setMessage("");
+  }
+  return (
+    <div className="contact-container">
+      <div
+        className="contact-section py-[6rem] px-[15%] bg-lightgray font-roboto relative"
+        id="Contact"
+      >
+        <div className="connect-box w-[40vw] flex flex-col gap-4">
+          <h1 className="text-pred text-[4rem] font-semibold">
+            Connect with me
+          </h1>
+          <input
+            type="text"
+            name="name"
+            id=""
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            value={name}
+            placeholder="Name"
+            className="bg-lightergray p-4 text-gray-500 outline-none"
+          />
+          <input
+            type="email"
+            name="email"
+            id=""
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            placeholder="Email"
+            className="bg-lightergray p-4 text-gray-500 outline-none"
+          />
+
+          <input
+            type="text"
+            name="msg"
+            id=""
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            value={message}
+            placeholder="Message"
+            className="bg-lightergray p-4 text-gray-500 outline-none"
+          />
+          <div className="flex w-full justify-end text-lightgray text-[1.2rem]">
+            <button
+              onClick={handleuser}
+              className="bg-pred p-4 flex gap-4 items-center justify-center font-bold"
+            >
+              Stay Connected
+              <FaRegArrowAltCircleRight />
+            </button>
+          </div>
+        </div>
+        <div className="line absolute w-[2px] bg-gray-400 top-[30%] right-[20%] h-[250px] max-[770px]:hidden">
+          <div className="circle1 w-8 h-8 border-2 border-pred bg-lightgray rounded-full absolute top-0 right-[-14.8px] z-2"></div>
+          <div className="circle2 w-8 h-8 border-2 border-pred bg-pred rounded-full absolute top-[43%] right-[-14.8px]"></div>
+          <div className="circle3 w-8 h-8 border-2 border-pred bg-lightgray rounded-full absolute bottom-0 right-[-14.8px]"></div>
+        </div>
+      </div>
+      <div className="copyright bg-black h-[3rem] flex justify-center items-center">
+        <p>&copy; Satyam | Developer | Programming Hunter 2023</p>
+      </div>
+    </div>
+  );
 }
 
 export default Service;
